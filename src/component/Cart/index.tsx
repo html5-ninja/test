1d2d93ee61a00f5944b3cb25832da507bd36180e
Zied Hosni <zied.hosni.mail@gmail.com>
2026-03-22 22:46:16 -0400

    test cart popover implementation

---
+++ src/component/Cart/index.tsx
@@ -0,0 +1,19 @@
+
+
+const Cart = () => {
+
+
+  return (
+    <div className="relative group">
+      <button className="btn" aria-label={"cart"}>
+        cart
+2
+        <div className="popover">
+product goes here
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Cart;
