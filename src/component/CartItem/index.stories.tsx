8cb04ccaea409f84851f5fad244fa4dc52ac70f3
Zied Hosni <zied.hosni.mail@gmail.com>
2026-03-23 16:54:02 -0400

    cartItem story

---
+++ src/component/CartItem/index.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CartItem from '.';
+
+const meta: Meta<typeof CartItem> = {
+  title: 'Components/CartItem',
+  component: CartItem,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof CartItem>;
+
+export const Default: Story = {
+  args: {
+    product: {
+      id: 'P001',
+      title: 'Flame Lizard Plush',
+      sku: 'PLUSH-FL-001',
+      price: 19.99,
+      quantity: 2,
+      points: 20,
+      variations: [],
+    },
+    stock: 120,
+    onRemove: () => null,
+    onUpdateQuantity: () => null
+  },
+};
